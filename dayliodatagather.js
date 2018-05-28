77c4d2aabb0f34f2f83d3492862c1dd5b5f23f3f
Valentin Ionita <valentin.ionita1201@stud.acs.upb.ro>
2018-05-28 19:50:25 +0300

    Functionality:JSON download + data formatting +  modularisation

--- dayliodatagather.js
+++ dayliodatagather.js
@@ -1,23 +1,41 @@
-// TODO: modularize
-// TODO: clear formatting
-// TODO: add download to .json
-
 function alert(alertclass) {
-  	$(alertclass).slideToggle();
-  	window.setTimeout(function() {
-	    $(alertclass).slideToggle("slow");
-		}, 2000);
+ 	$(alertclass).slideToggle();
+ 	window.setTimeout(function() {
+    $(alertclass).slideToggle("slow");
+	}, 2000);
+}
+
+function copyToClipboard() {
+	let $temp = $("<input>");
+
+	$("body").append($temp);
+	$temp.val($(".panel-body").html()).select();
+	document.execCommand("copy");
+	$temp.remove();
+
+	alert("#alert-copy");
+}
+
+function jsonDownload() {
+	let data = "data:text/json;charset=utf-8," + $(".panel-body").html(),
+		download = document.getElementById('dldLink');
+
+	download.setAttribute("href", data);
+	download.click();
+
+	alert("#alert-download");
 }
 
 function process() {
 	let moods = [0, 0, 0, 0, 0],
-		activities = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+		activities = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		firstTime = 1;
 
-	$('button', $('#moods')).each(function () {
-    $(this).click(function() {
-    	let elem = $(this).get(0),
-    		index = Array.prototype.indexOf.call(elem.parentNode.children, elem),
-    		initial = moods[index];
+	$(".panel-body").append("[]");
+
+	$('button', $('#moods')).each(function (index) {
+    $(this).click(function () {
+    	let initial = moods[index];
 
     	moods = Array.apply(null, Array(5)).map(Number.prototype.valueOf,0);
     	moods[index] = 1 - initial;
@@ -25,54 +43,40 @@ function process() {
     	$('button', $('#moods')).each(function() {
     		$(this).removeClass("btn-success");
     	});
+
     	if (moods[index] === 1) {
     		$("#moods button:nth-child(" + (index + 1) + ")").addClass("btn-success");
     	}
-
 		});
 	});
 
-	$('button', $('#activities')).each(function () {
-    $(this).click(function() {
+	$('button', $('#activities')).each(function (index) {
+    $(this).click(function () {
     	$(this).toggleClass("btn-primary");
-    	let elem = $(this).get(0),
-    		childIndex = Array.prototype.indexOf.call(elem.parentNode.children, elem),
-    		parentIndex = Array.prototype.indexOf.call(elem.parentNode.parentNode.children, elem.parentNode),
-    		index = parentIndex * 5 + childIndex;
-
-    	//console.log(index);
 			activities[index] = 1 - activities[index];
 		});
 	});
 
+	// set Date interaction
 	$('#date').click(function() {
-		if (!Date.parse($('.form-control').val())) {
-			alert("#alertdate");
-		} else {
-			alert("#alertdateset");
-		}
+		!Date.parse($('.form-control').val()) ? alert("#alertdate") :
+		alert("#alertdateset");
 	});
 
 	// copy to clipboard
-	$("#copy").click(function() {
-  	var $temp = $("<input>");
-  	$("body").append($temp);
-  	$temp.val($(".panel-body").text()).select();
-  	document.execCommand("copy");
-  	$temp.remove();
+	$("#copy").click(copyToClipboard);
 
-		alert(".alert-info");
-	});
+	// download as .json
+	$("#json").click(jsonDownload);
 
 	$("#submit").click(function() {
 		let inputValue = $('.form-control').val(),
 			date = new Date(inputValue),
-			inputDate = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
+			inputDate = date.getFullYear() + "-" + (date.getMonth()+1) + "-" +
+				date.getDate(),
 			mood = moods.findIndex(val => val > 0),
-			text = '{&#13;&#10;  "date:" ' + inputDate +
-				',&#13;&#10;  "mood": ' + (mood + 1) +
-				',&#13;&#10;  "activities": [' + activities.join() +
-				']&#13;&#10;}, ';
+			text = '{&#10;  "date": "' + inputDate + '",&#10;  "mood": ' +
+				(mood + 1) + ',&#10;  "activities": [' + activities.join() + ']&#10;}]';
 
 		if(!Date.parse(inputValue)) {
 			alert("#alertdate");
@@ -81,11 +85,23 @@ function process() {
 		} else if (mood == -1) {
 	  	alert("#alertmood");
 		} else {
+			// update the text, along with correct JSON formatting
+			$('.panel-body').text(function (_,txt) {
+			  return txt.slice(0, -1);
+			});
+
+			if(firstTime) {
+				firstTime = 0;
+			} else {
+				$(".panel-body").append(", ");
+			}
+
 			$(".panel-body").append(text);
 			
 			// increment date
 			date.setDate(date.getDate() + 1);
-			inputDate = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
+			inputDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" +
+				date.getDate();
 			$('.form-control').val(inputDate);
 
 			// reset 
