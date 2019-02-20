5b059972ec28025ebe3bac1a4be3c37eee97cbd8
vanntile <valentin.ionita1201@gmail.com>
2019-02-20 19:24:32 +0200

    MAG CH04

--- mostly-adequate-guide/exercises/ch04/exercise_c.js
+++ mostly-adequate-guide/exercises/ch04/exercise_c.js
@@ -1,8 +1,8 @@
 // Considering the following function:
 //
-//   const keepHighest = (x, y) => (x >= y ? x : y);
+// const keepHighest = (x, y) => (x >= y ? x : y);
 //
 // Refactor `max` to not reference any arguments using the helper function `keepHighest`.
 
 // max :: [Number] -> Number
-const max = xs => reduce((acc, x) => (x >= acc ? x : acc), -Infinity, xs);
+const max = reduce(keepHighest, -Infinity);
