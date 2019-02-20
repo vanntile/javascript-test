6d9d971c509a4e4884cf018f00fbfd4593d75ba7
vanntile <vanntile@protonmail.com>
2019-02-20 21:34:35 +0200

    MAG CH10

--- mostly-adequate-guide/exercises/ch10/exercise_a.js
+++ mostly-adequate-guide/exercises/ch10/exercise_a.js
@@ -1,4 +1,6 @@
 // Write a function that adds two possibly null numbers together using `Maybe` and `ap`.
 
 // safeAdd :: Maybe Number -> Maybe Number -> Maybe Number
-const safeAdd = undefined;
+const safeAdd = curry((x, y) => Maybe.of(add)
+  .ap(x)
+  .ap(y));
