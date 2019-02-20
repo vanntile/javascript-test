f40d5ac55dde4d56aba1b6b2892ce61181a7d692
vanntile <vanntile@protonmail.com>
2019-02-20 19:43:19 +0200

    MAG CH05

--- mostly-adequate-guide/exercises/ch05/exercise_c.js
+++ mostly-adequate-guide/exercises/ch05/exercise_c.js
@@ -2,8 +2,9 @@
 // Hint, the `flip` function may come in handy.
 
 // fastestCar :: [Car] -> String
-const fastestCar = (cars) => {
-  const sorted = sortBy(car => car.horsepower, cars);
-  const fastest = last(sorted);
-  return concat(fastest.name, ' is the fastest');
-};
+// const fastestCar = (cars) => {
+//   const sorted = sortBy(car => car.horsepower, cars);
+//   const fastest = last(sorted);
+//   return concat(fastest.name, ' is the fastest');
+// };
+const fastestCar = compose(append(' is the fastest'), prop('name'), last, sortBy(prop('horsepower')))
